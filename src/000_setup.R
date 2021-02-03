d279037be5165e131986cfe32a776ef596065bbf
BeNrn <56606686+BeNrn@users.noreply.github.com>
2021-02-03 16:59:05 +0100

    remove setup functionallity

--- src/000_setup.R
+++ src/000_setup.R
@@ -1,41 +1,35 @@
-#--------NEUE VERSION
-#installation über ---- devtools::install_github("envima/envimaR") ----
-# Set project specific subfolders
-
-project_folders = c("data/",                                 # data folders
-                                        "data/aerial/org/", "data/aerial/processed/",
-                                        "data/aerial/norm","data/lidar/org/", "data/lidar/clip/",
-                                        "data/lidar/processed/", "data/lidar/test/","data/grass/", "data/height_models",
-                                        "data/data_mof/", "data/tmp/", "data/valid/", "data/plots", "data/species_det",
-                                        "data/lidar/preprocessing/",
-                                        "run/", "log/", "data/lidar/org/corrected/",                         # bins and logging
-                                        "mpg-envinfosys-teams-2018-bjcm_rs_18/src/",      # source code
-                                        "mpg-envinfosys-teams-2018-bjcm_rs_18/doc/",
-                                        "data/lidar/preprocessing/level1",
-                                        "data/lidar/preprocessing/level2",
-                                        "data/lidar/preprocessing/level3",
-                                        "data/lidar/preprocessing/level4",
-                                        "data/lidar/preprocessing/level5",
-                                        "data/lidar/preprocessing/raster_level",
-                                        "data/lidar/preprocessing/raster_level/stdmetr",
-                                        "data/lidar/preprocessing/raster_level/shannon",
-                                        "data/lidar/preprocessing/raster_level/returns",
-                                        "data/indices", "data/species_comp")
-
-#Check for needed packages
-#required_packages <- c("envimaR", "lidR", "link2GI", "mapview", "raster", "rgdal","rlas", "sp",
-#                       "RStoolbox", "rlist","graphics", "ForestTools", "itcSegment",
-#                       "uavRst", "rLiDAR", "imager", "SpaDES.tools", "factoextra")
+#Skript for setting up a universal working environment on different computers
+#Setting up project specific subfolders
+#installation of devtools using devtools::install_github("envima/envimaR")
 
 
-#if (length(setdiff(required_packages, rownames(installed.packages()))) > 0){
-#  print(paste0(setdiff(required_packages, installed.packages()), "_package is not and will now be installed"))
-#  install.packages(setdiff(required_packages, installed.packages()))
-#} else {
-#  print("all packages are installed")
-#}
+project_folders = c("data/",
+                    "data/aerial/org/", "data/aerial/processed/","data/aerial/norm",
+                    "data/lidar/org/", "data/lidar/clip/","data/lidar/processed/",
+                    "data/lidar/test/","data/lidar/preprocessing/", "data/lidar/org/corrected/",
+                    "data/lidar/preprocessing/level1",
+                    "data/lidar/preprocessing/level2",
+                    "data/lidar/preprocessing/level3",
+                    "data/lidar/preprocessing/level4",
+                    "data/lidar/preprocessing/level5",
+                    "data/lidar/preprocessing/raster_level",
+                    "data/lidar/preprocessing/raster_level/stdmetr",
+                    "data/lidar/preprocessing/raster_level/shannon",
+                    "data/lidar/preprocessing/raster_level/returns",
+                    "data/grass/",
+                    "data/height_models",
+                    "data/data_mof/",
+                    "data/tmp/",
+                    "data/valid/",
+                    "data/plots",
+                    "data/species_det",
+                    "run/",
+                    "log/",
+                    "mpg-envinfosys-teams-2018-bjcm_rs_18/src/",
+                    "mpg-envinfosys-teams-2018-bjcm_rs_18/doc/",
+                    "data/indices",
+                    "data/species_comp")
 
-#rm(required_packages)
 
 # Set libraries
 libs = c("lidR", "link2GI", "mapview", "raster", "rgdal","rlas", "sp")
--- src/1_01_Processing_Optical_Data.R
+++ src/1_01_Processing_Optical_Data.R
@@ -1,21 +1,23 @@
 #Set working directory and load required packages
-library(envimaR)
-root_folder = alternativeEnvi(root_folder = "~/edu/mpg-envinsys-plygrnd",
-                              alt_env_id = "COMPUTERNAME",
-                              alt_env_value = "PCRZP",
-                              alt_env_root_folder = "F:\\BEN\\edu")
 
+library(raster)
+# library(envimaR)
+# root_folder = alternativeEnvi(root_folder = "~/edu/mpg-envinsys-plygrnd",
+#                               alt_env_id = "COMPUTERNAME",
+#                               alt_env_value = "PCRZP",
+#                               alt_env_root_folder = "F:\\BEN\\edu")
+# source(file.path(root_folder, "mpg-envinfosys-teams-2018-bjcm_rs_18/src/000_setup.R"))
+workingDir <- "D:/BEN/edu/data/"
 
-source(file.path(root_folder, "mpg-envinfosys-teams-2018-bjcm_rs_18/src/000_setup.R"))
-
-# search images
-list_files <- list.files(file.path(envrmt$path_aerial_org), pattern = ".tif")
+#search images
+#list_files <- list.files(file.path(envrmt$path_aerial_org), pattern = ".tif")
+list_files <- list.files(paste0(workingDir, "aerial/org/"), pattern = ".tif")
 list_files
 
-# load images
+#load images
 imagelist <- list()
 for (i in list_files) {
-  imagelist[[i]] <- brick(file.path(envrmt$path_aerial_org, i))
+  imagelist[[i]] <- brick(paste0(workingDir, "aerial/org/", i))
 }
 
 #rename files in list to assign every image to its position
@@ -39,8 +41,8 @@ if (length(unique(projct))==1){
 }
 
 #load shapefile
-shape1 <- readOGR(file.path(envrmt$path_data_mof, "AOI_mofshape.shp"),
-                  layer = ogrListLayers(paste(envrmt$path_data_mof, "AOI_mofshape.shp", sep = "/")))
+shape1 <- readOGR(paste0(workingDir, "data_mof/", "AOI_mofshape.shp"),
+                  layer = ogrListLayers(paste0(workingDir, "data_mof/", "AOI_mofshape.shp")))
 
 #check coordinate system of the shapefile
 crs(shape1)
@@ -52,13 +54,13 @@ crs(shape1) <- projct[[1]]
 cropped <- lapply(imagelist[3:length(imagelist)], crop, shape1)
 
 #mosaic overlapping images (IMPORTANT when using this for the first time -> set overwrite = T)
-imagelist3_4 <- mosaic(cropped$`3_476000_5630000.tif`, cropped$`4_476000_5630000_1.tif`, fun = min, overwrite = T, filename = file.path(envrmt$path_aerial_processed, "image3_4.tif"))
+imagelist3_4 <- mosaic(cropped$`3_476000_5630000.tif`, cropped$`4_476000_5630000_1.tif`, fun = min, overwrite = T, filename = paste0(workingDir, "aerial/processed/", "image3_4.tif"))
 
-imagelist5_6 <- mosaic(cropped$`5_476000_5632000.tif`, cropped$`6_476000_5632000_1.tif`, fun = min, overwrite = T, filename = file.path(envrmt$path_aerial_processed, "image5_6.tif"))
+imagelist5_6 <- mosaic(cropped$`5_476000_5632000.tif`, cropped$`6_476000_5632000_1.tif`, fun = min, overwrite = T, filename = paste0(workingDir, "aerial/processed/", "image5_6.tif"))
 
 #merging of the images (IMPORTANT when using this for the first time -> set overwrite = T)
-aoiimg <- merge(imagelist3_4, imagelist5_6, cropped$`7_478000_5630000.tif`, cropped$`8_478000_5632000.tif`, overwrite= T, filename=file.path(envrmt$path_aerial_processed, "AOIimage.tif"))
+aoiimg <- merge(imagelist3_4, imagelist5_6, cropped$`7_478000_5630000.tif`, cropped$`8_478000_5632000.tif`, overwrite= T, filename=paste0(workingDir, "aerial/processed/", "AOIimage.tif"))
 
 #plot of the optical data
-plotRGB(brick(file.path(envrmt$path_aerial_processed, "AOIimage.tif")))
+plotRGB(brick(paste0(workingDir, "aerial/processed/", "AOIimage.tif")))
 plot(shape1, add=TRUE)
